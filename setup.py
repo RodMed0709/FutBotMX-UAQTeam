a9b66d7af3db5c62ccf0e5903007d5d7c4d0ddda
legokna <leonardo_villanueva97@protonmail.com>
2026-05-29 15:09:28 -0600

    chore: setup package structure and init interfaces

---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name="futbot_core", version="0.1", packages=find_packages())
---
+++ src/core/__init__.py
@@ -0,0 +1,2 @@
+from .detector import ZeroShotDetector
+from .io import FrameGenerator
